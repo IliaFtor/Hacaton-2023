869812356f616493789b7c5dbb3bf5bd3e883bce
Bogdyans <130456237+Bogdyans@users.noreply.github.com>
2023-11-23 14:27:42 +0300

    Delete fullstack-backend/src directory

--- fullstack-backend/src/main/java/com/bogdyan/fullstackbackend/repository/DisciplineRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bogdyan.fullstackbackend.repository;
-
-import com.bogdyan.fullstackbackend.model.Discipline;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DisciplineRepository extends JpaRepository<Discipline, Integer> {
-}
